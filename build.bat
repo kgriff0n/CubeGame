34dd1add3dbc34dc4451303852c566f9aec8d721
KGriffon <kgriffob0t@gmail.com>
2025-10-07 21:08:37 +0200

    Fix building script

--- build.bat
+++ build.bat
@@ -1,5 +1,3 @@
-pyinstaller --onefile src/client.py
-pyinstaller --onefile src/server.py
-cp -r src/font dist/
-cp src/client.conf dist/
-cp src/server.conf dist/
+@echo off
+powershell -ExecutionPolicy Bypass -File "%~dp0build.ps1"
+pause
